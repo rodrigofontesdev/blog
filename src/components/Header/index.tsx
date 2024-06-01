d3dff021ceba9c5e287d2e2a95dda325f06a7897
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-01 13:10:55 -0300

    feat(header): create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import { HeaderContainer } from './styles'
+
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <img src={logoImg} alt="Github Blog" />
+      </Link>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+import headerBackgroundImg from '../../assets/header-background.png'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 296px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image: url(${headerBackgroundImg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding-right: 2rem;
+  padding-left: 2rem;
+  z-index: -1;
+
+  @media (max-width: 1024px) {
+    background-position: left;
+  }
+`
