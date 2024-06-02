1fd39cea524443b01df367b96668dec9baecfffb
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-01 23:45:33 -0300

    feat(page): insert profile card, search form and posts in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,33 @@
+import { PostCard } from './components/PostCard'
+import { ProfileCard } from './components/ProfileCard'
+import { Container, Posts, PostsGrid, SearchForm } from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <main>
+      <Container>
+        <ProfileCard />
+
+        <Posts>
+          <header>
+            <h2>Publicações</h2>
+            <span>6 publicações</span>
+          </header>
+
+          <SearchForm>
+            <input type="search" name="s" placeholder="Buscar conteúdo" />
+          </SearchForm>
+
+          <PostsGrid>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+          </PostsGrid>
+        </Posts>
+      </Container>
+    </main>
+  )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: calc(54rem + 4rem);
+  margin: 13rem auto 0;
+  padding-right: 2rem;
+  padding-left: 2rem;
+`
+
+export const Posts = styled.section`
+  padding-bottom: 15rem;
+
+  > header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.75rem;
+
+    h2 {
+      color: ${(props) => props.theme['slate-200']};
+      font-size: 1.125rem;
+    }
+
+    span {
+      color: ${(props) => props.theme['slate-400']};
+      font-size: 0.875rem;
+    }
+  }
+`
+
+export const SearchForm = styled.form`
+  display: flex;
+  margin-bottom: 3rem;
+
+  input {
+    flex: 1;
+    background-color: ${(props) => props.theme['slate-950']};
+    color: ${(props) => props.theme['slate-300']};
+    font-size: 1rem;
+    padding: 0.75rem 1rem;
+    border: 1px solid ${(props) => props.theme['slate-600']};
+    border-radius: 6px;
+
+    &::placeholder {
+      color: ${(props) => props.theme['slate-500']};
+    }
+
+    &:hover,
+    &:focus {
+      border-color: ${(props) => props.theme['blue-500']};
+      transition: color 400ms, border 400ms;
+    }
+  }
+`
+
+export const PostsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
