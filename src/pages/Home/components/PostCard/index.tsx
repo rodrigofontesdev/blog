e263950e5419ba3bf2e91d4665734bc88eac95ef
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-01 23:35:25 -0300

    feat(post-card): create post card component

---
+++ src/pages/Home/components/PostCard/index.tsx
@@ -0,0 +1,18 @@
+import { Card, Heading } from './styles'
+
+export function PostCard() {
+  return (
+    <Card>
+      <Heading>
+        <h3>JavaScript data types and data structures</h3>
+        <span>Há 1 dia</span>
+      </Heading>
+
+      <p>
+        Programming languages all have built-in data structures, but these often differ from one
+        language to another. This article attempts to list the built-in data structures available
+        in...
+      </p>
+    </Card>
+  )
+}
---
+++ src/pages/Home/components/PostCard/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Card = styled.article`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.25rem;
+  width: 100%;
+  background-color: ${(props) => props.theme['slate-700']};
+  padding: 2.375rem 2rem;
+  border-radius: 10px;
+`
+
+export const Heading = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  flex-wrap: wrap;
+  gap: 1rem;
+
+  h3 {
+    flex-grow: 1;
+    width: 80%;
+    font-size: 1.25rem;
+  }
+
+  span {
+    flex-shrink: 0;
+    color: ${(props) => props.theme['slate-400']};
+    font-size: 0.875rem;
+  }
+`
