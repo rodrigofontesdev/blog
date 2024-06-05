a783a69734ad05f340103dbc3f448b93ed74bf3d
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-05 20:04:07 -0300

    feat(page): get the issue from GitHub and render their content within single post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,13 +1,102 @@
+import { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
+import { useParams } from 'react-router-dom'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { lucario } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import remarkBreaks from 'remark-breaks'
+import remarkGfm from 'remark-gfm'
+import remarkGithub from 'remark-github'
+import { api } from '../../lib/axios'
 import { PostInfo } from './components/PostInfo'
 import { Container, PostBody } from './styles'
 
+interface Issue {
+  title: string
+  body: string
+  url: string
+  comments: number
+  createdAt: string
+  author: {
+    username: string
+    profileUrl: string
+  }
+}
+
+const repoOwner = import.meta.env.VITE_GITHUB_USERNAME
+const repoName = import.meta.env.VITE_GITHUB_REPO
+
 export function Post() {
+  const [issue, setIssue] = useState<Issue | null>()
+  const { postId } = useParams()
+
+  useEffect(() => {
+    async function getIssue() {
+      const response = await api.get(`/repos/${repoOwner}/${repoName}/issues/${postId}`)
+      const data = response.data
+
+      setIssue({
+        title: data.title,
+        body: data.body,
+        url: data.html_url,
+        comments: data.comments,
+        createdAt: data.created_at,
+        author: {
+          username: data.assignee.login,
+          profileUrl: data.assignee.html_url,
+        },
+      })
+    }
+
+    getIssue()
+  }, [postId])
+
   return (
     <main>
       <Container>
-        <PostInfo />
+        {issue && (
+          <PostInfo
+            title={issue.title}
+            url={issue.url}
+            comments={issue.comments}
+            author={issue.author}
+            createdAt={issue.createdAt}
+          />
+        )}
 
-        <PostBody></PostBody>
+        <PostBody>
+          <Markdown
+            remarkPlugins={[
+              remarkGfm,
+              remarkBreaks,
+              [remarkGithub, { repository: `${repoOwner}/${repoName}` }],
+            ]}
+            components={{
+              code(props) {
+                const { children, className, node, ref, ...rest } = props
+                const match = /language-(\w+)/.exec(className || '')
+
+                return match ? (
+                  <SyntaxHighlighter {...rest} PreTag="div" language={match[1]} style={lucario}>
+                    {String(children).replace(/\n$/, '')}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code className={className} {...rest}>
+                    {children}
+                  </code>
+                )
+              },
+              a({ node, children, ...props }) {
+                return (
+                  <a target="_blank" {...props}>
+                    {children}
+                  </a>
+                )
+              },
+            }}
+          >
+            {issue?.body}
+          </Markdown>
+        </PostBody>
       </Container>
     </main>
   )
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -10,5 +10,100 @@ export const Container = styled.div`
 
 export const PostBody = styled.article`
   width: 100%;
-  padding: 2.5rem 2rem;
+  padding: 2.5rem 2rem 10.5rem 2rem;
+
+  p,
+  pre,
+  ul,
+  ol {
+    margin-bottom: 1.5rem;
+  }
+
+  ul {
+    padding-left: 2rem;
+  }
+
+  ul.contains-task-list {
+    padding-left: 1rem;
+
+    li {
+      list-style: none;
+    }
+  }
+
+  ol {
+    padding-left: 2rem;
+
+    li {
+      list-style: decimal;
+    }
+  }
+
+  li {
+    margin-top: 0.25rem;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+  }
+
+  h4 {
+    font-size: 1rem;
+  }
+
+  h5 {
+    font-size: 0.875rem;
+  }
+
+  h6 {
+    font-size: 0.75rem;
+  }
+
+  a {
+    color: ${(props) => props.theme['blue-500']};
+  }
+
+  code {
+    background-color: ${(props) => props.theme['slate-500']};
+    padding: 2px 4px;
+    border-radius: 4px;
+  }
+
+  blockquote {
+    padding: 0 1rem;
+    border-left: 4px solid ${(props) => props.theme['slate-500']};
+  }
+
+  table {
+    table-layout: auto;
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+    margin-bottom: 1.5rem;
+
+    tr {
+      background-color: ${(props) => props.theme['slate-900']};
+      border-top: 1px solid ${(props) => props.theme['slate-700']};
+    }
+
+    tr:nth-child(2n) {
+      background-color: ${(props) => props.theme['slate-800']};
+    }
+
+    th,
+    td {
+      font-weight: 700;
+      text-align: left;
+      padding: 0.375rem 0.75rem;
+      border: 1px solid ${(props) => props.theme['slate-700']};
+    }
+  }
 `
