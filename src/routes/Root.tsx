7adcadb9109fbbb022832a1466a4a067394da92c
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-09 17:05:16 -0300

    refactor(router): change default error element

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { DefaultLayout } from '../layout/DefaultLayout'
+import { Error } from '../pages/Error'
 import { Home } from '../pages/Home'
 import { Post } from '../pages/Post'
 
@@ -7,6 +8,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <DefaultLayout />,
+    errorElement: <Error />,
     children: [
       { index: true, element: <Home /> },
       { path: '/post/:postId', element: <Post /> },
